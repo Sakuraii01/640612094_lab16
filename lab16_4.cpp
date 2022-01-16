aa4277650a7ab4f365dc4e49f6dcfc5657beaa9e
Sakuraii <pearl.k.sakura@gmail.com>
2022-01-16 22:16:36 +0700

    5 complete but 4 dont  know

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -6,7 +6,6 @@ using namespace std;
 void shuffle(int &,int &,int &,int &);
 
 int main(){
-	srand(time(0));
 	int a = 50, b = 100, c = 500, d = 1000;
 	
 	srand(time(0));	
@@ -19,8 +18,39 @@ int main(){
 	return 0;
 }
 
-void shuffle(int &i,int &j,int &k,int &m){
-	int ran = rand()%4+1;
+void shuffle(int &a,int &b,int &c,int &d){
+	int func[]={a,b,c,d};
+	int temp;
+	
+	int ran = rand()%4;
+	
+	for(int i = 3 ; i >= 0; i--){
+		temp = func[ran];
+		func[ran] = func[i];
+		func[i] = temp;
+	}
 
-	if(ran = 1) ;
+	/*while(ran == ran2) ran2 = rand()%4;
+	
+	temp = func[ran];
+	func[ran] = func[ran2];
+	func[ran2] = temp;
+	cout << func[0] <<endl;
+
+	
+		/*int ran = rand()%4+1;
+		if(ran==1){
+			i=j;
+			j=temp1;
+		}else if(ran==2){
+			j=k;
+			k=temp2;
+		}else if(ran == 3){
+			k=m;
+			m=temp3;
+		}else{
+			m=i;
+			i=temp4;
+		}*/
+	
 }
